85d20dc0a944eb2f418b2806044c9f1278638da1
Juan Cancela <cancela.juancarlos@gmail.com>
2016-01-28 15:30:51 -0300

    added form and a managers

--- resource/Index.js
+++ resource/Index.js
@@ -3,12 +3,14 @@
 var LinkFactory = require('../utils/LinkFactory').LinkFactory;
 var lf = new LinkFactory();
 
+var gameParameters = [{"name":"players", "descr":"Number of Players (1 to 6)"}];
+
 class Index {
     *render(ctx){
       yield this.render("Index",{
         controls : [
             lf.a("rules", "rules", this),
-            lf.a("game", "game", this),
+            lf.form(gameParameters, "test.js", "post" ),
             lf.a("self", null, this)
         ]
       });
--- utils/LinkFactory.js
+++ utils/LinkFactory.js
@@ -2,9 +2,29 @@
 
 class LinkFactory {
     
+    /**
+     * Creates HTML a links
+     * @param rel HTML a rel param
+     * @param href HTML a href param
+     * @param ctx context object
+     * 
+     * @returns an HTML a tag customized with the given parameters
+     */
     a(rel, href, ctx) {
         if(!href) href = "";
-        return {"rel":rel,"url":`${ctx.request.origin}/${href}`, "type":"a"}
+        return `<a rel=${rel} href="${ctx.request.origin}/${href}">${rel}</a>`; 
+    }
+    
+    form(inputParameters, action, method){
+        var _inputParameters = function(inputParameters){
+           var inputParametersString = "";
+           inputParameters.forEach(function(inputParameter){
+           inputParametersString += `${inputParameter.descr}:<br><input type="text" name="${inputParameter.name}"><br>`;
+        });  
+        return inputParametersString; 
+        }
+        
+        return `<form action="${action}"><br/>${_inputParameters(inputParameters)}<input type="submit" value="submit"></form>`    
     }
 }
 
